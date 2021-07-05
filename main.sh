e5ee77f4b594670d64be3bedec384420e1e70392
haselwimmer <christian@haselwimmer.co.uk>
2021-07-05 15:49:46 -0700

    Final project submission

--- main.sh
+++ main.sh
@@ -5,4 +5,5 @@ jupyter nbconvert --to notebook --execute notebooks/2-landsat-time-series.ipynb
 jupyter nbconvert --to notebook --execute notebooks/3-preprocess-rs-timeseries.ipynb
 jupyter nbconvert --to notebook --execute notebooks/4-meteorology-timeseries.ipynb
 jupyter nbconvert --to notebook --execute notebooks/5-data-integration-analysis.ipynb
-jupyter nbconvert --to html --no-input --output-dir='notebooks' --execute notebooks/6-final_blog.ipynb
+jupyter nbconvert --to notebook --execute notebooks/6-plotting.ipynb
+jupyter nbconvert --to html --no-input --output-dir='notebooks' --execute notebooks/7-final_blog.ipynb
